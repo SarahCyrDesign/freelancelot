68fcb87f2f96c047ba92677c4f4515ebf38bd002
sarahcyr <github email address>
2017-09-19 11:00:25 -0400

    Finish user migration and start user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+    include Pundit
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    protect_from_forgery with: :exception
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    private
+    def after_sign_in_path_for(resource)
+      request.env['omniauth.origin'] || root_path
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referrer || root_path)
+    end
+
+    def respond_to_html_and_json(variable)
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: variable}
+      end
+    end
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_initialize :set_default_role
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
+
+  enum role: [:user, :client, :admin]
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+
 end
