45847143170585430a19936aee4e66251a1f017c
sarahcyr <github email address>
2017-10-04 07:30:32 -0400

    Add category and project_category models

--- db/migrate/20170919165811_create_projects.rb
+++ db/migrate/20170919165811_create_projects.rb
@@ -1,7 +1,18 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-
+      t.string :title
+      t.integer :freelancer_id
+      t.integer :category_id
+      t.text :description
+      t.string :client_name
+      t.boolean :invoice_sent, default: false
+      t.string :status, default: 'Received'
+      t.integer :budget
+      t.integer :time_log DEFAULT 0
+      t.date :start_date
+      t.date :deadline
+      t.integer :ticket
       t.timestamps null: false
     end
   end
