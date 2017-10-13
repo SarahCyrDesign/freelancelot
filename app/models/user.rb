4007890bc4880fb5707c643e583e13224ce1a113
sarahcyr <github email address>
2017-10-13 07:56:42 -0400

    Implement Facebook login feature by refactoring callback controller and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,17 @@ class User < ActiveRecord::Base
   has_many :projects, :foreign_key => 'freelancer_id'
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+    @user = find_by(provider: auth.provider, uid: auth.uid)
+    if @user
+      @user
+    else
+      @user = self.create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.name = auth.info.name
+        user.password = Devise.friendly_token[0,20]
+      end
     end
   end
 
