b5f9669d5405350fb326da214800c52224c03757
sarahcyr <github email address>
2017-10-18 08:23:43 -0400

    Modify seed data for new users to login immediately with completed projects and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,55 +5,56 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email:"test@test.com", password:"12345678")
-User.create(email:'test2@test.com', password:'12345678')
-User.create(email: 'freelancer1@test.com', password: '12345678' role: 0, id: 1)
-User.create(email: 'freelancer2@test.com', password: '12345678' role: 0, id: 2)
-User.create(email: 'freelancer3@test.com', password: '12345678' role: 0, id: 3)
-User.create(email: 'client@test.com', password: '12345678' role: '1', id: 4)
-User.create(email: 'admin@test.com', password: '12345678' role: '2', id: 5)
+User.create(name: "Test1", email:"test@test.com", password:"12345678")
+User.create(name: "Test2", email:'test2@test.com', password:'12345678')
+User.create(name: "Freelancer1", email: 'freelancer1@test.com', password: '12345678', role: 'freelancer', id: 1)
+User.create(name: "Freelancer2",email: 'freelancer2@test.com', password: '12345678', role: 'freelancer', id: 2)
+User.create(name: "Freelancer3", email: 'freelancer3@test.com', password: '12345678', role: 'freelancer', id: 3)
+User.create(name: "Client", email: 'client@test.com', password: '12345678', role: 'client', id: 4)
+User.create(name: "Admin", email: 'admin@test.com', password: '12345678', role: 'admin', id: 5)
 
-Category.create(name: 'Graphic Design', id: 1)
-Category.create(name: 'Illustration', id: 2)
-Category.create(name: 'Video Editing', id: 3)
+Category.create(name: 'Graphic Design', id: 1, user_id: 1)
+Category.create(name: 'Illustration', id: 2, user_id: 2)
+Category.create(name: 'Video Editing', id: 3, user_id: 3)
+
 
   Project.create(title: "Tigerzord logo",
          freelancer_id: 1,
          category_id: 1,
-         text: 'A simple 4 color logo in vectors',
+         description: 'A simple 4 color logo in vectors',
          client_name: 'White Ranger',
          invoice_sent: false,
          status: 'In Progress',
          budget: 50,
          time_log: 5,
-         start_date: Date.new("2017,9,1"),
-         deadline: Date.new("2017,10,1"),
+         start_date: Date.new(2017,9,1),
+         deadline: Date.new(2017,10,1),
          ticket: 001)
 
 
   Project.create(title: "Unicorn Painting",
          freelancer_id: 2,
          category_id: 2,
-         text: 'Oil Painting of a majestic Unicorn',
+         description: 'Oil Painting of a majestic Unicorn',
          client_name: 'Rainbow Dash',
          invoice_sent: true,
          status: 'Completed',
          budget: 200,
          time_log: 20,
-         start_date: Date.new("2017,8,10"),
-         deadline: Date.new("2017,9,1"),
+         start_date: Date.new(2017,8,10),
+         deadline: Date.new(2017,9,1),
          ticket: 333)
 
 
   Project.create(title: "Family Fishing Trip",
          freelancer_id: 3,
          category_id: 3,
-         text: '10 montage video with color editing',
+         description: '10 montage video with color editing',
          client_name: 'Hank Hill',
-         invoice_sent: false,
+         invoice_sent: true,
          status: 'Received',
          budget: 20,
          time_log: 0,
-         start_date: Date.new("2017,10,1"),
-         deadline: Date.new("2017,11,15),
+         start_date: Date.new(2017,10,1),
+         deadline: Date.new(2017,11,15),
          ticket: 5299)
