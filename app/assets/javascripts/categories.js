5c4dba6a0eb472007f19a47668dab58251ec8039
sarahcyr <github email address>
2017-11-27 12:22:29 -0500

    Clear out unneeded comments from views. In category.js clear the form after adding new project in JSON

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -2,13 +2,14 @@
 //create a new category
 $(function () {
   $('form').submit(function(e) {
-    console.log("submitting")
     e.preventDefault();
     var values = $(this).serialize();
     var posting = $.post('/categories', values);
     posting.done(function(data) {
       var category = data;
       $("#categoryName").text(category["name"]);
+      //clear out the input form
+      $('input[type="text"], textarea').val('');
     });
   });
 });
@@ -17,10 +18,8 @@ $(function () {
 $(function () {
   $(".js-next").on("click", function(e) {
     e.preventDefault()
-    console.log("next button clicked")
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/categories/" + nextId + ".json", function(data) {
-      console.log(data)
       var category = data;
       $(".categoryName").text(category["name"]);
       // re-set the id to current on the link
